d4f1b7c7d5337c9fd39b73964ffd5f310d9a0bda
jozsa <allisonjoyweiner@gmail.com>
2019-06-01 22:05:32 +0000

    Added custom User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import RegexValidator
 
-# Create your models here.
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                             message="Mobile number must be entered in the format:"
+                                     " '+999999999'. Up to 15 digits allowed.")
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email):
+        """
+        Creates and saves User
+        """
+        if not email:
+            raise ValueError('Email is required')
+
+        user = self.model(email=MyUserManager.normalize_email(email),
+                )
+
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
+    email_verified = models.BooleanField(default=False)
+
+    #mobile = models.CharField(validators=[phone_regex], max_length=15, unique=True, blank=True, null=True)
+    #mobile_verified = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
