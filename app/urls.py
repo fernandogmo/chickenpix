d4e0c0778113b06c66ab45e71e5eefd0525db519
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-06-02 23:14:41 +0000

    Added models and urls connecting apps

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index'),
+        path('', TemplateView.as_view(template_name='home.html'), name='home'),
+        path('accounts/', include('django.contrib.auth.urls')),
+        path('', include('drfpasswordless.urls')),
+        path('success/', views.email_auth_success, name='email_auth_success'),
+        path('failure/', views.email_auth_failure, name='email_auth_failure'),
+        path('photos/', views.photos, name='photos'),
 ]
