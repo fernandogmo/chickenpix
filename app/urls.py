bd2b7d81a05820baa4fb295e962beb92c646992f
jozsa <allisonjoyweiner@gmail.com>
2019-06-02 02:58:19 +0000

    Experimenting with navbar - need to make a template for that. Added preliminary file upload functionality

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
         path('', TemplateView.as_view(template_name='home.html'), name='home'),
         path('accounts/', include('django.contrib.auth.urls')),
-        path('accounts/', include('drfpasswordless.urls')),
+        path('', include('drfpasswordless.urls')),
         path('success/', views.email_auth_success, name='email_auth_success'),
         path('failure/', views.email_auth_failure, name='email_auth_failure'),
         path('photos/', views.photos, name='photos'),
