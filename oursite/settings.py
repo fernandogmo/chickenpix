91cf7fc36485e154a1d9b858dfbe32458769e8d0
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-06-03 00:23:24 +0000

    Reverted to blank ALLOWED_HOSTS

--- oursite/settings.py
+++ oursite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6vjk-e9@up&u2#vswqujb@!^rv9hku_f2l^3y6s+aixg_jzhq7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['55.55.55.5', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
