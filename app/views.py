c594b52ade6501054d0e333e01b7859a297bf98f
jozsa <allisonjoyweiner@gmail.com>
2019-06-03 23:53:46 +0000

    Working authentication/login structure. Needs to be refactored.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.contrib.auth.decorators import login_required
 from .forms import EmailForm, TokenForm
 import requests
+import datetime
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 # Create your views here.
 def index(request):
@@ -12,19 +16,22 @@ def index(request):
 def success(request):
     if request.method == 'GET':
         form = TokenForm()
+        message = request.GET.get("message", "")
     else:
         form = TokenForm(request.POST)
         if form.is_valid():
             response = requests.post('http://0.0.0.0:8000/callback/auth/', data={'token': request.POST.get("token", "")})
             if response.status_code == 200:
-                if request.user.email_verified:
-                    login(request, request.user)
-                    return redirect('/')
-                else:
-                    return redirect('/')
+                token = response.json().get('token') 
+                user_token = Token.objects.get(key=token)
+                user = User.objects.get(id=user_token.user_id)
+                login(request, user)
+                return redirect('/')
             else:
-                return render(request, 'failure.html')
-    return render(request, 'success.html', {'form': form})
+                return redirect('/')
+        else:
+            return render(request, 'failure.html')
+    return render(request, 'success.html', {'form': form, 'message': message})
 """
 def failure(request):
     return render(request, 'failure.html')
@@ -41,7 +48,7 @@ def home(request):
         if form.is_valid():
             response = requests.post('http://0.0.0.0:8000/auth/email/', data={'email': request.POST.get("email", "")})
             if response.status_code == 200:
-                return redirect('/success')
+                return redirect('/success', message=response.json().get('detail'))
             else:
-                return render(request, 'failure.html')
-    return render(request, 'home.html', {'form': form})
+                return render('/', {'message': response.json().get('detail')})
+    return render(request, 'home.html', {'form': form, 'message': 'You are not logged in. Please enter your email address to receive a login token. No signup is required!'})
